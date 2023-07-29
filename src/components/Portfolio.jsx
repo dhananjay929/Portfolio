4ac7231e35ab4f26e11f1bbdae9e30345bf6f92d
Dhananjay Kumar <dhananjaykjcs@gmail.com>
2023-07-29 21:16:29 +0530

    logos changed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import textanalyzer from "../assets/text-analyzer.png"
-import calculator from "../assets/calculator.png"
-import ecommerce from "../assets/e-commerce.png"
-import moviereviewer from "../assets/movie-reviewer.png"
-import tictactoe from "../assets/tic-tac-toe.png"
-import newsapp from "../assets/news-app.png"
+import textanalyzer from "../assets/Text.png"
+import calculator from "../assets/Calc.png"
+import ecommerce from "../assets/E-Com.png"
+import moviereviewer from "../assets/Movie.png"
+import tictactoe from "../assets/Tic-tac.png"
+import newsapp from "../assets/News.png"
 
 const Portfolio = () => {
     const portfolios = [
@@ -16,7 +16,7 @@ const Portfolio = () => {
         {id:6,demolink:"https://dhananjay929.github.io/E-Commerce/",codelink:"https://github.com/dhananjay929/E-Commerce" ,src: ecommerce}
     ]
   return (
-    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white lg,md:h-screen'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
@@ -29,7 +29,7 @@ const Portfolio = () => {
             portfolios.map(({id,src,demolink,codelink})=>(
 
                
-                    <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                    <div key={id} className='shadow-md shadow-gray-500 rounded-lg'>
                              <img src={src} alt="" className='rounded-md duration-200 hover:scale-105'/>
                  <div className='flex items-center justify-center'>
                            <a href={demolink}  target='_blank' rel="noreferrer" className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110'>Demo</a>
